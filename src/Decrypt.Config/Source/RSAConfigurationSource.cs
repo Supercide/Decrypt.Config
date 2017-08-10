ad93fcc567e1e94fbb10e2f479d2ae40bd499c96
Jordan anderson <jordan_anders@live.co.uk>
2017-08-10 17:50:30 +0100

    Adds RSA configuration source proivder stub

---
+++ src/Decrypt.Config/Source/RSAConfigurationSource.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decrypt.Config.Source
+{
+    class RSAConfigurationSource : JsonConfigurationSource
+    {
+    }
+}
